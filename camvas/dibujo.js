7ce07149001e8e2aea784cb6a8ebe134b9dd429a
Mario Fernandez Serrano <me.fernandezserrano@gmail.com>
2020-03-01 18:24:55 -0500

    Funciones en JS

--- camvas/dibujo.js
+++ camvas/dibujo.js
@@ -1,12 +1,16 @@
 var d = document.getElementById("dibujos");
 var lienzo = d.getContext("2d");
 
+dibujarLineas("#ff2b95", 150, 150, 300, 300)
+dibujarLineas("#1a1a1a", 50, 150, 150, 220)
 
-lienzo.beginPath();
-lienzo.strokeStyle = "green";
+function dibujarLineas(color, xinicial, yinicial, xfinal, yfinal) {
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
 
-lienzo.moveTo(150, 150);
-lienzo.lineTo(300, 300);
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, xfinal);
 
-lienzo.stroke();
-lienzo.closePath();
+  lienzo.stroke();
+  lienzo.closePath();
+}
