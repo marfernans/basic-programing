9e9ad5ffce9a4022c79c1e47a9e7ba627891abb6
Mario Fernandez Serrano <me.fernandezserrano@gmail.com>
2020-03-01 23:09:49 -0500

    Ciclos while y for en JavaScript

--- camvas/dibujo.js
+++ camvas/dibujo.js
@@ -1,15 +1,42 @@
 var d = document.getElementById("dibujos");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var colory = "#ffa700";
 
-dibujarLineas("#ff2b95", 150, 150, 300, 300)
-dibujarLineas("#1a1a1a", 50, 150, 150, 220)
+
+// while (l < lineas) {
+//   yi = 10 * l;
+//   xf = 10 * (l + 1);
+
+//   dibujarLineas(colorx, 0, yi, xf, 300);
+
+//   console.log("lineas " + l);
+
+//   l = l + 1;
+// }
+
+
+for (l = 0; l < lineas; l++) {
+  yi = 10 * l;
+  xf = 10 * (l + 1);
+
+  dibujarLineas(colory, 0, yi, xf, 300);
+  dibujarLineas(colory, 300, yi, xf, 0);
+
+  console.log("lineas " + l);
+}
+
+dibujarLineas(colory, 1, 1, 1, 300);
+dibujarLineas(colory, 1, 299, 299, 299);
 
 function dibujarLineas(color, xinicial, yinicial, xfinal, yfinal) {
   lienzo.beginPath();
   lienzo.strokeStyle = color;
 
   lienzo.moveTo(xinicial, yinicial);
-  lienzo.lineTo(xfinal, xfinal);
+  lienzo.lineTo(xfinal, yfinal);
 
   lienzo.stroke();
   lienzo.closePath();
