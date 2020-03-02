aaa07fedc749308d8f115d0d6c59d754e37d4db6
Mario Fernandez Serrano <me.fernandezserrano@gmail.com>
2020-03-02 15:34:16 -0500

    Eventos y Formularios en HTML y JavaScript

--- camvas/dibujo.js
+++ camvas/dibujo.js
@@ -1,35 +1,10 @@
+var texto = document.getElementById("texto__lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click" , dibujoPorClick);
+
 var d = document.getElementById("dibujos");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var colory = "#ffa700";
-
-
-// while (l < lineas) {
-//   yi = 10 * l;
-//   xf = 10 * (l + 1);
-
-//   dibujarLineas(colorx, 0, yi, xf, 300);
-
-//   console.log("lineas " + l);
-
-//   l = l + 1;
-// }
-
-
-for (l = 0; l < lineas; l++) {
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-
-  dibujarLineas(colory, 0, yi, xf, 300);
-  dibujarLineas(colory, 300, yi, xf, 0);
-
-  console.log("lineas " + l);
-}
-
-dibujarLineas(colory, 1, 1, 1, 300);
-dibujarLineas(colory, 1, 299, 299, 299);
 
 function dibujarLineas(color, xinicial, yinicial, xfinal, yfinal) {
   lienzo.beginPath();
@@ -37,7 +12,27 @@ function dibujarLineas(color, xinicial, yinicial, xfinal, yfinal) {
 
   lienzo.moveTo(xinicial, yinicial);
   lienzo.lineTo(xfinal, yfinal);
-
   lienzo.stroke();
   lienzo.closePath();
+}
+function dibujoPorClick() {
+  var lineas = parseInt(texto.value);
+  var l = 0;
+  var yi, xf;
+  var colory = "#ffa700";
+
+  var espacio = ancho / lineas;
+ 
+
+  for (l = 0; l < lineas; l++) {
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLineas(colory, 0, yi, xf, 300);
+    console.log("lineas " + l);
+  }
+
+  dibujarLineas(colory, 1, 1, 1, 299);
+  dibujarLineas(colory, 1, 299, 299, 299);
+
+  
 }
