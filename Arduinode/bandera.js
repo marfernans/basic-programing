7dd903314191126cf5b11f208e1d7e7b35a37a15
Mario Fernandez Serrano <me.fernandezserrano@gmail.com>
2020-03-08 19:24:33 -0500

    Robot bandera programado con JavaScriot y NodeJS

---
+++ Arduinode/bandera.js
@@ -0,0 +1,40 @@
+var j5 = require("johnny-five");
+var circuito = new j5.Board();
+var bonbi, motor, celda;
+var turno = 0;
+
+circuito.on("ready", prende);
+
+function prende() {
+  var configuracion = {
+    pin: "A0",
+    freq: 50
+  };
+  celda = new j5.Sensor(configuracion);
+
+  bonbi = new j5.Led(13);
+  bonbi.on();
+
+  motor = j5.Servo(9);
+
+  motor.to(90);
+  ondear();
+}
+
+function ondear() {
+  console.log("Luz: " + celda.value);
+
+  var luz = celda.value;
+  if (luz > 10) {
+    if (turno == 1) {
+      turno = 0;
+      motor.to(70);
+    } else {
+      turno = 1;
+      motor.to(110);
+    }
+  } else {
+    motor.to(150);
+  }
+  setTimeout(ondear, 1000);
+}
